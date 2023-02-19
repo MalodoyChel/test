aa40e29790512ab0f94c4892f15e87fc6acc2d63
sardorgafarov <gafarovsardorn1@gmail.com>
2023-02-19 23:10:05 +0500

    finish

--- Sardor'sMain.js
+++ Sardor'sMain.js
@@ -1,21 +1,24 @@
-let backMod = document.querySelector(".SardorBackModal"),
-    saveMod = document.querySelector(".SardorModSave"),
-    openMod = document.querySelector(".SardorOpenMod"),
-    nameInp = document.querySelector(".SardorInpName"),
-    mailInp = document.querySelector(".SardorInpMail"),
-    roleInp = document.querySelector(".SardorInpRole"),
-    profileInp = document.querySelector(".SardorInpProfile"),
-    telInp = document.querySelector(".SardorInpTel"),
-    userName = document.querySelector(".SardorNames"),
-    userName1 = document.querySelector(".SardorNames1"),
-    userProfile = document.querySelector(".SardorProfiles"),
-    userProfile1 = document.querySelector(".SardorProfiles1"),
-    userMail = document.querySelector(".SardorMail"),
-    userJob = document.querySelector(".SardorJob"),
-    userStatus = document.querySelector(".SardorStatus"),
-    userTel = document.querySelector(".SardorTel"),
-    form = document.querySelector(".SardorModTopRight"),
+let backMod = document.querySelector(".Sardor_Back_Modal"),
+    saveMod = document.querySelector(".Sardor_Mod_Save"),
+    openMod = document.querySelector(".Sardor_Open_Mod"),
+    nameInp = document.querySelector(".Sardor_Inp_Name"),
+    mailInp = document.querySelector(".Sardor_Inp_Mail"),
+    roleInp = document.querySelector(".Sardor_Inp_Role"),
+    profileInp = document.querySelector(".Sardor_Inp_Profile"),
+    telInp = document.querySelector(".Sardor_Inp_Tel"),
+    userName = document.querySelector(".Sardor_Names"),
+    userName1 = document.querySelector(".Sardor_Names1"),
+    userProfile = document.querySelector(".Sardor_Profiles"),
+    userProfile1 = document.querySelector(".Sardor_Profiles1"),
+    userMail = document.querySelector(".Sardor_Mail"),
+    userJob = document.querySelector(".Sardor_Job"),
+    userStatus = document.querySelector(".Sardor_Status"),
+    userTel = document.querySelector(".Sardor_Tel"),
+    form = document.querySelector(".Sardor_Mod_Top_Right"),
     inps = document.querySelectorAll("input[name]"),
+    showHideMail = document.querySelector(".Sardor_Img1"),
+    showHideRole = document.querySelector(".Sardor_Img2"),
+    showHideProfile = document.querySelector(".Sardor_Img3"),
     result = {};
 
 function local(key, value) { localStorage.setItem(key, value) };
@@ -24,8 +27,35 @@ openMod.addEventListener("click", () => {
     backMod.style.display = "flex"
 });
 backMod.addEventListener("click", (e) => {
-    if ((e.target.classList.contains("SardorBackModal")) || (e.target.classList.contains("SardorModExit"))) {
+    if ((e.target.classList.contains("Sardor_Back_Modal")) || (e.target.classList.contains("Sardor_Mod_Exit"))) {
         backMod.style.display = "none"
+    }
+});
+showHideMail.addEventListener("click", () => {
+    if (mailInp.getAttribute("type") == "password") {
+        mailInp.setAttribute("type", "email")
+        showHideMail.setAttribute("src", "./Sardor'sImages/icon-unlock.png")
+    } else {
+        mailInp.setAttribute("type", "password")
+        showHideMail.setAttribute("src", "./Sardor'sImages/icon-lock.png")
+    }
+});
+showHideRole.addEventListener("click", () => {
+    if (roleInp.getAttribute("type") == "password") {
+        roleInp.setAttribute("type", "text")
+        showHideRole.setAttribute("src", "./Sardor'sImages/icon-unlock.png")
+    } else {
+        roleInp.setAttribute("type", "password")
+        showHideRole.setAttribute("src", "./Sardor'sImages/icon-lock.png")
+    }
+});
+showHideProfile.addEventListener("click", () => {
+    if (profileInp.getAttribute("type") == "password") {
+        profileInp.setAttribute("type", "text")
+        showHideProfile.setAttribute("src", "./Sardor'sImages/icon-unlock.png")
+    } else {
+        profileInp.setAttribute("type", "password")
+        showHideProfile.setAttribute("src", "./Sardor'sImages/icon-lock.png")
     }
 });
 saveMod.addEventListener("click", (e) => {
